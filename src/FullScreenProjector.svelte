d829c1d50730b009aa15db4aee8de8fda387b985
Florian Roulet <florianroulet@gmail.com>
2025-01-20 22:24:18 +0100

    feat() : WIP add monitor list

--- src/FullScreenProjector.svelte
+++ src/FullScreenProjector.svelte
@@ -3,53 +3,41 @@
   import { sendCommand } from './obs.js'
   import SourceButton from './SourceButton.svelte'
 
+  let monitors = [];
+  let monitorIndex = 0;
+
   onMount(async function () {
-    const data = await sendCommand('GetMonitorList')
-    // SourceFilterListReindexed ==> sourceName
-    // SceneItemCreated ==> sourceUuid
-    // GetSceneItemSource ==> sourceName, sourceUuid
-    // GetMonitorList monitorIndex
-
-
-    console.log('GetMonitorList', data)
+    let data = await sendCommand('GetMonitorList')
+    monitors = data.monitors || []
+    console.log('GetMonitorList', monitors)
   })
-  function sceneClicker (scene) {
+
+  function buttonClicker () {
     return async function () {
-      await sendCommand('OpenVideoMixProjector', { videoMixType: "OBS_WEBSOCKET_VIDEO_MIX_TYPE_PROGRAM", monitorIndex: 1 })
+      await sendCommand('OpenVideoMixProjector', { videoMixType: "OBS_WEBSOCKET_VIDEO_MIX_TYPE_PROGRAM", monitorIndex })
     }
   }
 </script>
 
-<ol>
+<div>
   FullScreenProjector
-  <SourceButton name="FullScreenProjector"
-                on:click={sceneClicker()}/>
-</ol>
+  <select bind:value={monitorIndex} title="Select fullScreenProjector destination">
+    {#each monitors as monitor}
+      <option value={monitor.monitorIndex}>{monitor.monitorName}</option>
+    {/each}
+  </select>
+  <SourceButton
+    name="FullScreenProjector"
+    on:click={buttonClicker()}
+  />
+</div>
 
 <style>
-  ol {
-    list-style: None;
+  div {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
     gap: .5rem;
     margin-bottom: 2rem;
-  }
-  ol.column {
-    flex-direction: column;
-    gap: 1rem;
-  }
-  li {
-    display: inline-block;
-    min-width: 10rem;
-    flex-grow: 1;
-  }
-  ol.with-icon {
-    justify-content: center;
-  }
-  ol.with-icon li {
-    min-width: 0;
-    flex-grow: 0;
-    flex-shrink: 1;
   }
 </style>
